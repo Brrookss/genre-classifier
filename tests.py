c801de5ad8b205bd38b10d6af6ddd95eff3f0561
Brooks Burns <burns.brooks@outlook.com>
2022-06-20 14:46:57 -0700

    Add tests for functions associated with dataset preprocessing

--- tests.py
+++ tests.py
@@ -2,13 +2,42 @@
 
 import unittest
 
+from dataset.preprocess.encode import integer_encode_mapping
+from dataset.preprocess.encode import is_sorted
+from dataset.preprocess.features import audio_to_image_representation
 
-class TestCase(unittest.TestCase):
 
-    def testStringMatches_HelloWorld(self):
-        input = "Hello World"
-        expected = "Hello World"
-        self.assertEqual(input, expected)
+class TestDatasetPreprocess(unittest.TestCase):
+
+    def testIntegerEncodeMapping_CorrectMapping(self):
+        input = ["a", "b", "c", "d", "e"]
+        expected = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
+        self.assertEqual(integer_encode_mapping(input), expected)
+
+    def testIsSorted_InputSorted(self):
+        input = [0, 1, 2, 3, 4, 5]
+        self.assertTrue(is_sorted(input))
+
+    def testIsSorted_InputNotSorted(self):
+        input = [5, 4, 3, 2, 1, 0]
+        self.assertFalse(is_sorted(input))
+
+    def testIsSorted_InputPartiallyNotSorted(self):
+        input = [0, 2, 1, 3, 4, 5]
+        self.assertFalse(is_sorted(input))
+
+    def testIsSorted_InputEmpty(self):
+        input = []
+        self.assertTrue(is_sorted(input))
+
+    def testIsSorted_InputDuplicateSorted(self):
+        input = [0, 0, 1, 2, 3, 4, 5]
+        self.assertTrue(is_sorted(input))
+
+    def testAudioToImageRepresentation_CorrectShape(self):
+        input = [[0], [0]]
+        expected = (1, 2, 1)
+        self.assertEqual(audio_to_image_representation(input).shape, expected)
 
 
 if __name__ == "__main__":
